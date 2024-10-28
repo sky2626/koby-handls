2e0cecbfaaa7c273f8713d894d8b9d2d43551139
Salu Alhassan <alhassansalu1@gmail.com>
2024-10-28 20:33:51 +0000

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
-    <div class="flex justify-center items-center gap-4 flex-col rounded-2xl p-8 shadow-lg">
+    <div class="flex justify-center items-center gap-4 flex-col rounded-2xl p-4 shadow-lg">
         <div class="h-[100px] w-[100px] ">
             <img class="rounded-full" src="/public/logo.jpeg" alt="image of brand logo">
         </div>
         <h1 class="text-xl font-bold text-center text-black">Kobby West Couture
         </h1>
-        <div class=" ">
-            <a href="https://x.com/West336195Kobby" class="bg-black">
-                X
+        <div class="flex flex-col gap-2 max-w-xl">
+            <a href="https://x.com/West336195Kobby"
+                class="w-full bg-black rounded p-1 text-lg font-semibold text-center">
+                <iconsX />
+                Follow us on x
             </a>
+            <a href="https://www.instagram.com/kobbywestcouture/"
+                class="w-full bg-gradient-to-r from-[#f9ce34] via-[#ee2a7b] to-[#6228d7]  rounded p-1 text-lg font-semibold text-center">
+                <iconsX />
+                Follow us on Instagram
+            </a>
+            
         </div>
     </div>
 </template>
